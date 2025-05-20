0f64813388d040dcba7158b6182d7fb089948926
AlanYamami <20223TN046@utez.edu.mx>
2025-05-20 15:51:11 -0600

    feat/repos

---
+++ Back_Carros/src/main/java/mx/edu/utez/back_carros/BackCarrosApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.back_carros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCarrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackCarrosApplication.class, args);
+    }
+
+}
